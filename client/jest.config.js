b97865c358567846c8c5bba7a16e57dbcfd59ee9
Rishabh Shah <35496282+rishabhshah13@users.noreply.github.com>
2024-08-06 23:11:26 -0400

    Removed all testing code, added no codepass

--- client/jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  // Other configurations...
-  transformIgnorePatterns: [
-    "node_modules/(?!(axios)/", // This regex tells Jest to transform axios within node_modules
-  ],
-};
