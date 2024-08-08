85aa928d6722b2908fb21264a4b6b270a36e5ac5
Rishabh Shah <35496282+rishabhshah13@users.noreply.github.com>
2024-08-08 18:56:09 -0400

    Changes

--- app/api/app.py
+++ app/api/app.py
@@ -22,12 +22,12 @@ app = FastAPI()
 
 # System prompt to guide the AI responses and maintain ethical interactions
 system_prompt = {
-  "role": "system",
-  "content": """You are an AI-powered virtual tour guide. Your main function is to provide informative and engaging narrations about various locations and answer user questions related to these locations. Adhere to the following guidelines strictly:\n\n1. Ensure all responses are informative and relevant to the location being discussed.\n2. Provide accurate and up-to-date information based on the available data for each location.\n3. Maintain a friendly and engaging tone while interacting with users.\n4. Avoid giving advice or opinions on topics outside the scope of the tour content.\n5. Respect user privacy and confidentiality. Do not request, store, or share personal information.\n6. Handle user queries respectfully and encourage a positive experience.\n7. If a query is outside the scope of the provided information or the tour, respond with a polite acknowledgment.\n8. Focus on enhancing the user experience by providing clear, relevant, and helpful information about the tour locations.\n\nExamples of how to handle specific types of questions, provide responses based on the tour data only:\n\n- Location Information: \"Here is what I can tell you about this location.\"\n- User Queries about the Tour: \"I can provide more details about the current location.\"\n- Requests for Unrelated Information: \"I’m here to provide information about the tour locations.\"\n- Follow-up Questions: \"I’m happy to provide more details on this topic if it relates to the tour.\"\n\nAlways maintain these principles to ensure a helpful and enjoyable tour experience.
-  I am going to ask you questions, answer the questions based on this data only. """
+    "role": "system",
+    "content": """You are an AI-powered virtual tour guide. Your main function is to provide informative and engaging narrations about various locations and answer user questions related to these locations. Adhere to the following guidelines strictly:\n\n1. Ensure all responses are informative and relevant to the location being discussed.\n2. Provide accurate and up-to-date information based on the available data for each location.\n3. Maintain a friendly and engaging tone while interacting with users.\n4. Avoid giving advice or opinions on topics outside the scope of the tour content.\n5. Respect user privacy and confidentiality. Do not request, store, or share personal information.\n6. Handle user queries respectfully and encourage a positive experience.\n7. If a query is outside the scope of the provided information or the tour, respond with a polite acknowledgment.\n8. Focus on enhancing the user experience by providing clear, relevant, and helpful information about the tour locations.\n\nExamples of how to handle specific types of questions, provide responses based on the tour data only:\n\n- Location Information: \"Here is what I can tell you about this location.\"\n- User Queries about the Tour: \"I can provide more details about the current location.\"\n- Requests for Unrelated Information: \"I’m here to provide information about the tour locations.\"\n- Follow-up Questions: \"I’m happy to provide more details on this topic if it relates to the tour.\"\n\nAlways maintain these principles to ensure a helpful and enjoyable tour experience.
+  I am going to ask you questions, answer the questions based on this data only. """,
 }
 
-LLAMAFILE_SERVER = os.environ.get('LLAMAFILE_SERVER', 'http://localhost:8080')
+LLAMAFILE_SERVER = os.environ.get("LLAMAFILE_SERVER", "http://localhost:8080")
 
 
 # Add CORS middleware to handle cross-origin requests
@@ -45,6 +45,7 @@ app.add_middleware(
     ],  # Allow any headers. Update this to restrict specific headers if needed.
 )
 
+
 # Define the schema for the request body
 class Query(BaseModel):
     question: str
@@ -55,6 +56,7 @@ class Query(BaseModel):
         question (str): The user's question.
         model (str): The model to be used for generating a response. 
     """
+
 
 # Dependency function to validate the query parameters
 def validate_query(question: str, model: str) -> Query:
@@ -85,8 +87,10 @@ def validate_query(question: str, model: str) -> Query:
         )
     return Query(question=question, model=model)
 
+
 # Mount the static directory to serve files
 app.mount("/image", StaticFiles(directory="../../"), name="assets")
+
 
 # Endpoint to get a response based on the model specified in the query
 @app.post("/get_response")
@@ -114,8 +118,6 @@ async def get_response(query: Query):
         return await get_mistral_response(query)
 
 
-
-
 # Endpoint to get a response from GPT-4o model
 @app.post("/chatgpt")
 async def get_gpt4o_response(query: Query):
@@ -137,7 +139,7 @@ async def get_gpt4o_response(query: Query):
     completion = client.chat.completions.create(
         model="gpt-4o-mini-2024-07-18",
         messages=[
-            system_prompt ,  # Add the system prompt to guide the response
+            system_prompt,  # Add the system prompt to guide the response
             {
                 "role": "user",
                 "content": query.question,
@@ -148,20 +150,25 @@ async def get_gpt4o_response(query: Query):
     # Return the answer from GPT-4o
     return {"answer": completion.choices[0].message}
 
+
 # Endpoint to receive and store data as a global variable
 @app.post("/data")
 async def upload_segments(segments: List[str]):
-
     try:
         # await get_mistral_response(Query(question='Hello', model='Mistral'))
 
         global system_prompt
         # Process the segments received
         # You can perform various operations like saving to a file, database, etc.
-        system_prompt['content'] = system_prompt['content'] + ' '.join(segments)
+        system_prompt["content"] = system_prompt["content"] + " ".join(segments)
         # Return a success response
-        return JSONResponse(content={"message": f"Segments received successfully {LLAMAFILE_SERVER}/v1" }, status_code=200)
-    
+        return JSONResponse(
+            content={
+                "message": f"Segments received successfully {LLAMAFILE_SERVER}/v1"
+            },
+            status_code=200,
+        )
+
     except Exception as e:
         # Handle any exceptions that occur
         raise HTTPException(status_code=500, detail=str(e))
