aa4113dd4d5fea37216da2fdeac8271a83ef02ea
Rishabh Shah <35496282+rishabhshah13@users.noreply.github.com>
2024-08-08 00:38:37 -0400

    Working UI

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/Styles.css'; // Import the CSS file
+// import './components/Styles.css'; // Import the CSS file
 import DeepgramTTS from './components/DeepgramTTS';
 import TopBar from './components/TopBar';
 // src/index.js or src/App.js
@@ -16,16 +16,3 @@ root.render(
     <DeepgramTTS />
   </React.StrictMode>
 );
-
-// // src/index.js
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './components/Styles.css'; // Import the CSS file
-// import DeepgramTTS from './components/DeepgramTTS';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <DeepgramTTS />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
